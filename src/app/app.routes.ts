2f8e6d96a46fb7fe702a5bdee79372c9beec3f7b
Ja-ci <130248891+Ja-ci@users.noreply.github.com>
2025-01-16 12:31:15 +0300

    Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },  // Default route to HomeComponent
-  { path: '**', redirectTo: '' },          // Redirect unknown routes to home
+  { path: 'home', component: HomeComponent },  // Default route to HomeComponent
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },          // Redirect unknown routes to home
 ];
